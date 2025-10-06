750180b8b18290d8fa043cc6831b243f70449dba
“sagenxt” <sagenxtsolutions@gmail.com>
2025-10-06 21:33:04 +0530

    Updated Db credenatils

--- src/models/index.js
+++ src/models/index.js
@@ -3,10 +3,11 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'inspection_db',
   process.env.DB_USER || 'postgres',
-  '',
+  process.env.DB_PASS || 'postgres',
   {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
+      port: process.env.DB_PORT || 5432,
     logging: false,
   }
 );
