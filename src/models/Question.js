852938d714b9e4faa3434bffc5652d7788ce5fe8
Sri Vejan <sreddy@ptcg.in>
2025-10-15 08:53:57 +0530

    Updated questions length

--- src/models/Question.js
+++ src/models/Question.js
@@ -4,7 +4,7 @@ const { sequelize } = require('./index');
 module.exports = (sequelize, DataTypes) => {
   const Question = sequelize.define('Question', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    text: { type: DataTypes.STRING, allowNull: false },
+    text: { type: DataTypes.STRING(50000), allowNull: false },
     sectionId: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'Sections', key: 'id' } }
   });
   Question.associate = models => {
